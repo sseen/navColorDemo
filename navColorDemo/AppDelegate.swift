a15c43c6e2cf24e07473b0f883361a0bdc11e382
sseen <ssnfee@gmail.com>
2017-09-12 17:58:08 +0800

    果然不行

--- navColorDemo/AppDelegate.swift
+++ navColorDemo/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = UIColor(red: 70.0/255.0, green: 164.0/255.0, blue: 239.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.clear
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        
         return true
     }
 
